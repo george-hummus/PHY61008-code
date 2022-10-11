004eaa3442f733cbb58a135402c48fb6e80bf393
hummus <ghume1@sheffield.ac.uk>
2022-10-11 18:32:02 +0100

    New vis.py takes in TNS CSV file of alerts. Old ver has been archived

--- Visiblity/Version-Hist/vis-v0.py
+++ Visiblity/Version-Hist/vis-v0.py
@@ -1,5 +1,5 @@
 """
-Script to find the visibility of transients at a given position and date from their RA and Dec from the TNS alert CSV file.
+Script to find the visibility of transients at a given position and date from their RA and Dec.
 
 Author: George Hume
 2022
@@ -12,7 +12,6 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import numpy as np
 import json
-import csv
 import argparse
 
 parser = argparse.ArgumentParser(description = """
@@ -26,7 +25,13 @@ parser.add_argument('lat' , type = float, help = 'Lattitude of the follow-up tel
 parser.add_argument('long' , type = float, help = 'Longitude of the follow-up telescope.')
 parser.add_argument('elv' , type = float, help = 'Elevation above sea-level of the follow-up telescope.')
 
-parser.add_argument('tns_alerts' , type = str, help = 'File name of CSV file containing the alerts.')
+parser.add_argument('ra_hours' , type = int, help = 'Hours of RA of transient.')
+parser.add_argument('ra_mins' , type = int, help = 'Mintues of RA of transient.')
+parser.add_argument('ra_secs' , type = float, help = 'Seconds of RA of transient.')
+
+parser.add_argument('dec_degs' , type = int, help = 'Degrees of declination of transient.')
+parser.add_argument('dec_mins' , type = int, help = 'Arcmintues of declination of transient.')
+parser.add_argument('dec_secs' , type = float, help = 'Arcseconds of declination of transient.')
 
 args = parser.parse_args()
 
@@ -43,25 +48,9 @@ earth, sun = eph['earth'], eph['sun']
 location = wgs84.latlon(args.lat * N, args.long * E, elevation_m = args.elv)
 Epos = earth + location #sets up observing position (i.e., the postion of the follow-up telescope)
 
-#ingest the CSV File
-alerts=open(args.tns_alerts) #opens the csv file
-csvreader = csv.reader(alerts) #opens file as csv
-csv_date = next(csvreader) #first row which is the date the CSV file was created
-headers = next(csvreader) #second row contains the headers of the CSV file
-#now need to extract all the rows and save as numpy array
-rows = []
-for row in csvreader:
-        rows.append(row)
-rows = np.array(rows,dtype="object")
-
-#turn the rows into dictonaries so they are easily indexable
-tar_list = [] #list that will contain the dictonaries of each target
-for i in range(rows.shape[0]): #loop over each entry
-    target = rows[i]
-    prop_dict = {} #dict to be filled with targets properties
-    for j in range(rows.shape[1]): #loop over each element
-        prop_dict[headers[j]] = target[j] #save each property using key from headers
-    tar_list.append(prop_dict)
+#cretes star object from RA and Dec which represents the transient
+transient =  Star(ra_hours=(args.ra_hours, args.ra_mins, args.ra_secs),
+                   dec_degrees=(args.dec_degs, args.dec_mins, args.dec_secs))
 
 #need the local midday and the next day midday to calculate the dark time
 t0 = ts.from_datetime(date)
@@ -85,49 +74,23 @@ darkend = times[4]
 sunrise = times[7] #using the indcies to extract the different times of night
 nightphases = [sunset,darkstart,darkend,sunrise] #savetogether as list for when calculating alts
 
-all_dict = {} #dictonary to contain results form all targets
+#find altitude of transient at sunset, start of dark time, end of darktime, and sunrise
+altitudes = []
+for i in nightphases:
+    astro = Epos.at(i).observe(transient)
+    app = astro.apparent()
+    #observers star at time from position
+    alt, az, distance = app.altaz()
+    altitudes.append(alt.degrees)
 
-#find altitude of each transient at sunset, start of dark time, end of darktime, and sunrise
-for k in tar_list:
-    #RA and Dec of target in decimal degrees
-    RA = float(k["ra"])
-    dec = float(k["declination"])
-
-    #convert to RA(h:m:s) & Dec(d:m:s)
-    RAh = RA/15
-    ra_hours = int(RAh)
-    RAm = (RAh - ra_hours)*60
-    ra_mins = int(RAm)
-    ra_secs = (RAm - ra_mins)*60
-
-    dec_degs = int(dec)
-    DECm = (dec - dec_degs)*60
-    dec_mins = int(DECm)
-    dec_secs = (DECm - dec_mins)*60
-
-    #cretes star object from RA and Dec which represents the transient
-    transient =  Star(ra_hours=(ra_hours, ra_mins, ra_secs),
-                       dec_degrees=(dec_degs, dec_mins, dec_secs))
-    altitudes = []
-    for l in nightphases:
-        astro = Epos.at(l).observe(transient)
-        app = astro.apparent()
-        #observers star at time from position
-        alt, az, distance = app.altaz()
-        altitudes.append(alt.degrees)
-
-    #making dictonary that contains the outputs
-    Toutput = {"name":k['name_prefix']+k['name'],"RA":f"{ra_hours}:{ra_mins}:{ra_secs}","Dec":f"{dec_degs}:{dec_mins}:{dec_secs}","altitudes":{"sunset":altitudes[0],"darktime-start":altitudes[1],"darktime-end":altitudes[2],"sunrise":altitudes[3]}
-    }
-
-    all_dict[k["objid"]] = Toutput
-
+#making dictonary that contains the outputs
 output = {"location":{"lattitude":args.lat,"longitude":args.long,"elevation":args.elv},
 "date":args.date,
 "night_phases":{"sunset":sunset.utc_strftime("%H:%M:%S %d/%m/%Y"),"darktime-start":darkstart.utc_strftime("%H:%M:%S %d/%m/%Y"),"darktime-end":darkend.utc_strftime("%H:%M:%S %d/%m/%Y"),"sunrise":sunrise.utc_strftime("%H:%M:%S %d/%m/%Y")},
-"targets":all_dict
+"target":{"name":"transient1","RA":f"{args.ra_hours}:{args.ra_mins}:{args.ra_secs}","Dec":f"{args.dec_degs}:{args.dec_mins}:{args.dec_secs}",
+"altitudes":{"sunset":altitudes[0],"darktime-start":altitudes[1],"darktime-end":altitudes[2],"sunrise":altitudes[3]}}
 }
 
 #save output dict as json
-with open('transients.json', 'w') as fp:
+with open(f'transient1.json', 'w') as fp:
         json.dump(output, fp,indent=4)
