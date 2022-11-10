bdb171bb63652da570735cee51fabb496c7adc00
hummus <ghume1@sheffield.ac.uk>
2022-11-10 12:33:00 +0000

    obscuring

--- TNS/tns_update.bash
+++ TNS/tns_update.bash
@@ -3,7 +3,7 @@
 yesterday=$(date -u --date="yesterday" +'%Y%m%d') #date yesterday in utc
 
 #download the update
-curl -X POST -H 'user-agent: tns_marker{"tns_id":142993,"type": "bot", "name":"BillyShears"}' -d 'api_key=b9d40748683e621a019ffd4e472fa862b2f20755' https://www.wis-tns.org/system/files/tns_public_objects/tns_public_objects_${yesterday}.csv.zip > /home/pha17gh/TNS/tns_public_objects_${yesterday}.csv.zip
+curl -X POST -H 'user-agent: tns_marker{"tns_id":142993,"type": "bot", "name":"BillyShears"}' -d 'api_key=SECRET' https://www.wis-tns.org/system/files/tns_public_objects/tns_public_objects_${yesterday}.csv.zip > /home/pha17gh/TNS/tns_public_objects_${yesterday}.csv.zip
 
 #unzip update and remove archive
 unzip /home/pha17gh/TNS/tns_public_objects_${yesterday}.csv.zip -d /home/pha17gh/TNS/
